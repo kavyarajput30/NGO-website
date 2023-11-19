18d6f5f8ef1a396b6d8519635d3856c52b9b8a41
kavya rajput <131941640+kavyarajput30@users.noreply.github.com>
2023-11-19 18:57:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ window.addEventListener("scroll", function () {
 
 
 let firstBox = document.getElementById("first-box");
-let images = ['1.jpeg', '2.jpg' ,'3.jpg']; // Add more image URLs
+let images = [ '3.jpg','5.jpg' , '8.jpg', '9.jpg' ,'10.jpg']; // Add more image URLs
 var index = 0;
 
 function changeBackground() {
@@ -24,4 +24,30 @@ function changeBackground() {
 changeBackground();
 
 // Change the background image every 5 seconds
-setInterval(changeBackground, 3000);
+setInterval(changeBackground, 2000);
+
+
+
+
+
+
+  
+document.addEventListener("DOMContentLoaded", function () {
+    const container = document.getElementById("tenth-box-first-div");
+  
+    // Clone images for infinite slider effect
+    const images = container.querySelectorAll("img");
+    const wrapper = document.createElement("div");
+    wrapper.id = "slider-wrapper";
+  
+    images.forEach((img) => {
+      const clone = img.cloneNode(true);
+      wrapper.appendChild(clone);
+    });
+  
+    console.log(wrapper); // Add this line to check the content of the wrapper
+  
+    container.appendChild(wrapper);
+  });
+  
+  
